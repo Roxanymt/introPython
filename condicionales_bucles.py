e9985815a3f9205550c1dd56d34ccee24eb9fa20
Roxany Montenegro <roxanymontenegro@gmail.com>
2022-09-06 09:26:35 -0300

    condicionales

---
+++ condicionales_bucles.py
@@ -0,0 +1,13 @@
+#Condicionales
+x = 20
+if x > 10:  #los dos putnos marcan donde comienzan las instrucciones.
+    print("Mayor a 10")
+else:
+    print("Menor a 10")
+
+if x < 10:
+    print("Menor a 10")
+elif x > 25:
+    print("Mayor a 25")
+else:
+    print("Número entre 20 y 25")
