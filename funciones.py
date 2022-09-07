36ebc4867c5b7f20e32df8acd72e6711c139f160
Roxany Montenegro <roxanymontenegro@gmail.com>
2022-09-07 10:45:11 -0300

    added functions

--- funciones.py
+++ funciones.py
@@ -51,6 +51,20 @@ total_sumatoria = sumatoriaArray([2, 3, 4, 5]) #total_sumatoria = 14
 print(total_sumatoria)
 
 #RETO 2: Crear una función que reciba un arreglo y que regrese el premedio de los elementos del arreglo.
+#array = [1,2,3]
+#sum = 0
+#num = 1
+#sum = 0 + 1 = 1
+
+#num = 2
+#sum = 1 + 2 = 3
+
+#num = 3
+#sum = 3 + 3 = 6
+
+#cant_promedio = 6/3 = 2 
+
+#RETURN 2
 
 def promedio(array):
     sum = 0
@@ -63,6 +77,20 @@ def promedio(array):
     return cant_promedio
 
 
-total_promedio = promedio([1,2,3])
+total_promedio = promedio([1,2,3]) # total_promedio = 2
 
 print(total_promedio)
+
+#RETO 3: Crear una función que reciba un arreglo que sume solo los números postitivos.
+
+def sumPositivos(array):
+    sum = 0
+
+    for num in array:
+        if num > 0:  #solo suma 1 + 2 + 3, porque son positivos.
+            sum += num
+
+    return sum
+
+total_suma = sumPositivos ([1,2,3, -1]) #Solo suma positivos 
+print(total_suma)
